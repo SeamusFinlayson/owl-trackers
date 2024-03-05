cda28f7cb8997e94a004c7e8b1ca9071d4fa53a9
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2024-03-04 16:49:09 -0800

    Fix using DOM properties instead of JSX in svg

--- src/action/Action.tsx
+++ src/action/Action.tsx
@@ -68,13 +68,13 @@ export function Action(): JSX.Element {
               fill="#ffffff"
               transform="rotate(0)"
               stroke="#ffffff"
-              stroke-width="0.00048000000000000007"
+              strokeWidth="0.00048000000000000007"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
                 {" "}
